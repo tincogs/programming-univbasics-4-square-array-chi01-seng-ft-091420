29f1f9c3cb146243cadf4a002b64d33aff5dcbfc
Tom ODay <tomoday91@gmail.com>
2020-09-03 18:53:37 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
+numbers = [1,2,3]
 def square_array(array)
-  # your code here
+  squares = []
+  array.length.times { |index|
+    squares.push(array[index] ** 2)}
+  squares
 end
+square_array(numbers)
+new_numbers = [9,10,16,25]
+square_array(new_numbers)
